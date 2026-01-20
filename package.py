54ae1286531cc6f6094d6215bc5be429fa945bdd
206520318_nbcuni <jeff.mahovsky@dreamworks.com>
2026-01-20 10:39:24 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.37'
+    _version = '14.38'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -81,7 +81,7 @@ for i, variant in enumerate(variants):
     variant.insert(0, '.moonshine_variant-%d' % i)
 
 requires = [
-    'moonray-17.35',
+    'moonray-17.36',
     'scene_rdl2-15.22',
 ]
 
