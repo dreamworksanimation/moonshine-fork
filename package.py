c5fffaf578b52439b0c3201c4b643b7d4ae47dee
206519669_nbcuni <jon.lanz@dreamworks.com>
2026-03-10 19:31:41 -0700

    Bumped major version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.42'
+    _version = '15.0'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -81,8 +81,8 @@ for i, variant in enumerate(variants):
     variant.insert(0, '.moonshine_variant-%d' % i)
 
 requires = [
-    'moonray-17.40',
-    'scene_rdl2-15.24',
+    'moonray-18.0',
+    'scene_rdl2-16.0',
 ]
 
 private_build_requires = [
